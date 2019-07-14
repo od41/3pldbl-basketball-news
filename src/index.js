7a30bb9bddf52d1096db4dbc32cee21cd5cecb5d
od41 <odafe41@gmail.com>
2019-07-14 20:05:20 +0100

    created file structure for news app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// COMPONENTS
+import Header from './componenets/header'
+
 const App = () => {
-    return <h1>Hellow WOrld</h1>
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
 }
 
 ReactDOM.render(<App/>, document.querySelector("#root"))
