f6cf38fdc7aefbf4b13a6dd626d5ccf6c71bd914
od41 <odafe41@gmail.com>
2019-07-23 13:47:20 +0100

    more styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,64 @@
+/** @jsx jsx */
 import React from 'react'
-import '../css/styles.css'
+import { jsx } from '@emotion/core'
+import logo_url from '../assets/img/logo.png'
 
 const Header = (props) => {
 
-        return (
-            <header>
-                <img src="img/logo.png" className="logo" />
-                <button id="update" className="update-btn button">Crash the boards</button>
-                <input type='text' onChange={props.keywords}/>
-            </header>
-        )
+    // colours
+    const $brandPrimary = '#334036'
+    const $brandPrimaryDarker = '#1a201b'
+    const $brandBackground = '#F6F7FB'
+
+    // font sizes
+    const $bodyFont = '.9em'
+    const $h3 = '1.2em'
+    const $h2 = '1.6em'
+    const $h1 = '3em'
+
+    // styling
+    const header = {
+        padding: '30px 4em',
+        display: 'grid',
+        gridTemplateColumns: '30% auto',
+        width: 'calc(100% - 8em)',
+        boxShadow: '0px 1px 4px rgba(0,0,0,0.1)',
+        background: '#fff',
+        alignContent: 'center',
+        justifyContent: 'space-between'
+    }
+
+    const logo = {
+        width: '140px',
+        minWidth: '130px',
+        height: 'auto'
+    }
+
+    const updateBtn = {
+        alignSelf: 'center',
+        background: $brandPrimary,
+        color: 'white',
+        padding: '5px 20px',
+        border: 'none',
+        height: '40px',
+        borderRadius: '25px',
+        '&:hover': {
+            zIndex: 1,
+            cursor: 'pointer',
+            boxShadow: '0px 1px 5px rgba(0,0,0,0.6)',
+            backgroundColor: $brandPrimaryDarker
+    
+        }
+    }
+
+
+    return (
+        <header css={header}>
+            <img src={logo_url} css={logo} alt="3PLDBL Logo" />
+            <button id="update" css={updateBtn} >Crash the boards</button>
+            <input type='text' onChange={props.keywords}/>
+        </header>
+    )
     
 }
 
