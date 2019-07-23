e4cb83de48a404408ecd05d4d92abc310d9c036b
od41 <odafe41@gmail.com>
2019-07-23 04:35:32 +0100

    styled the news item properly + installed emotion

--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -1,26 +1,71 @@
+/** @jsx jsx */
 import React from 'react'
-import { css } from 'glamor'
+// import { css } from 'glamor'
+import { jsx } from '@emotion/core'
 
 const NewsItem = ({item}) => {
 
-    let news_item = css({
-        padding: '20px',
-        boxSizing: 'border-box',
-        borderBottom: '1px solid grey'
-    })
+    // colours
+    const $brandPrimary = '#334036'
+    const $brandPrimaryDarker = '#1a201b'
+    const $brandBackground = '#F6F7FB'
 
-    let news_grey = css({
-        ':hover': {
-            color: 'red',
-        }
-    })
+    // font sizes
+    const $bodyFont = '.9em'
+    const $h3 = '1.2em'
+    const $h2 = '1.6em'
+    const $h1 = '3em'
+   
+
+    const list_item = {
+        background: '#fff',
+        margin: '2em 0',
+        padding: '2em',
+        display: 'grid',
+        gridTemplateColumns: '40% auto',
+        gridTemplateRows: 'auto',
+        gridTemplateAreas: `
+                            "news-image title"
+                            "news-image excerpt"
+                            "news-image read-more"
+                            `
+      
+    }
+
+    
+    const image = {
+        gridArea: 'news-image'
+   
+    }
+
+    const title = {
+        gridArea: 'title',
+        fontSize: $h2,
+        marginBottom: '.5em'
+    }
+
+    const excerpt = {
+        gridArea: 'excerpt',
+        fontSize: $bodyFont,
+        textAlign: 'justify',
+        marginBottom: '.5em'
+    }
+
+    const read_more = {
+        gridArea: 'read-more',
+        textTransform: 'uppercase',
+        letterSpacing: '2px',
+        texDecoration: 'none',
+        fontWeight: 'bold',
+        color: $brandPrimary
+    }
 
     return(
-        <div {...news_item} className="news" id={item.id}>
-            <img src="https://picsum.photos/200" className="news-image" />
-            <h3 {...news_grey}>{item.title}</h3>
-            <p>{item.feed}</p>
-            <a href="#" target="_blank" rel="noopener noreferrer" class="read-more">Read more</a>
+        <div css={list_item} className="news" id={item.id}>
+            <img src="https://picsum.photos/200" {...image} />
+            <h3 css={title}>{item.title}</h3>
+            <p css={excerpt}>{item.feed}</p>
+            <a css={read_more} href="#" target="_blank" rel="noopener noreferrer" >Read more</a>
         </div>
         
     )
